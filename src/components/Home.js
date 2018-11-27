3ff2601bed7e89e8ae5f294759ba8d0363d664e2
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2018-11-27 15:10:35 -0500

    switch and img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component{
     state = {
@@ -19,8 +21,11 @@ class Home extends Component{
             posts.map(post =>{
                 return (
                     <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="A pokeball" />
                         <div className="card-content">
-                            <span className="card-title">{post.tile}</span>
+                            <Link to={'/'+post.id}>
+                                 <span className="card-title red-text">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
@@ -31,7 +36,7 @@ class Home extends Component{
         )
 
         return (
-            <div className="container">
+            <div className="containern home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
