22750f7af4c9fe672a0f1b2a7836e716e48ccd50
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2018-11-27 14:19:20 -0500

    react axios

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Contact = () =>{
+const Contact = (props) =>{
     return (
         <div className="container">
             <h4 className="center">Contact</h4>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React,{Component} from 'react'
+import axios from 'axios';
 
-const Home = () =>{
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-        </div>
-    )
+class Home extends Component{
+    state = {
+        posts:[]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+             .then(res =>{
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+             })
+    }
+    render(){
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post =>{
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.tile}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ):(
+            <div className="center">no post yet </div>
+        )
+
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
+    
 }
 
 export default Home
